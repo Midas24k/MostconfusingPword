bd7fc544e3155102f378e5b65e77aa00d464ef3e
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2023-11-16 23:32:32 -0500

    debugged script.js

--- script.js
+++ script.js
@@ -5,71 +5,79 @@ var lowercaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 var numbArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var symbolArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
 var allChars = []; //charactersLowercase + charactersUppercase + characternumb + charactersymbol
-var charLength = "";
+// var charLength = "";
 
 function generatePassword() {
+  console.log('SOMETHING HAS CHANGED');
 
   // prompt user "do you want to use lowercase letters?" or something
   console.log("you clicked the button ");
   // prompt user for length of password
   var charLength = prompt("Choose a number of characters between 8 and 128");
   charLength = parseInt(charLength);
+  console.log("charLength", charLength);
   if (charLength < 8 || charLength > 128) {
     alert("Did not meet criteria, please try again") // something telilng the user they did not follow the rules and need to try again
-    return '';
-  }
-  var password = "";
+    generatePassword();
+  } else {
 
-  var charactersLowercase = confirm("Would you like to use lowercase letters?");
-  console.log(charactersLowercase)
-  if (charactersLowercase == false) {
-    console.log(charactersLowercase);
-    // add lowercase letters to the allChars array
-    allChars = allChars.concat(lowercaseArr)
-    // add a random lc letter to password to ensure one is chosen
-    password += lowercaseArr[Math.floor(Math.random() * lowercaseArr.charLength)]
-  }
-  // prompt user something like "Uppercase letters?"
+    var password = "";
 
-  var charactersUppercase = confirm("Would you like to use Uppercase letters?");
-  console.log(charactersUppercase)
-  if (charactersUppercase == false) {
+    var charactersLowercase = confirm("Would you like to use lowercase letters?");
+    console.log(charactersLowercase)
+    if (charactersLowercase == true) {
+      console.log(charactersLowercase);
+      // add lowercase letters to the allChars array
+      allChars = allChars.concat(lowercaseArr)
+      console.log(allChars)
+      // add a random lc letter to password to ensure one is chosen
+      password += lowercaseArr[Math.floor(Math.random() * lowercaseArr.length)]
+      console.log(lowercaseArr[Math.floor(Math.random() * lowercaseArr.length)])
+    }
+    // prompt user something like "Uppercase letters?"
+
+    var charactersUppercase = confirm("Would you like to use Uppercase letters?");
     console.log(charactersUppercase)
-    // add uppercase chars to the allChars array
-    allChars = allChars.concat(UppercaseArr)
-    password += UppercaseArr[Math.floor(Math.random() * UppercaseArr.charLength)]
-  }
-  // prompt for nums?
+    if (charactersUppercase == true) {
+      console.log(charactersUppercase)
+      // add uppercase chars to the allChars array
+      allChars = allChars.concat(UppercaseArr);
+      console.log(allChars);
+      password += UppercaseArr[Math.floor(Math.random() * UppercaseArr.length)]
+    }
+    // prompt for nums?
 
-  var characternumb = confirm("would you like to use numbers?");
-  console.log(characternumb)
-  if (characternumb ==  false) {
+    var characternumb = confirm("would you like to use numbers?");
     console.log(characternumb)
-    // add to allChars
-    allChars = allChars.concat(numbArr)
-    password += numbArr[Math.floor(Math.random() * numbArr.charLength)]
+    if (characternumb ==  true) {
+      console.log(characternumb)
+      // add to allChars
+      allChars = allChars.concat(numbArr)
+      password += numbArr[Math.floor(Math.random() * numbArr.length)]
 
+    }
+    // prompt for special chars?
+
+    var charactersymbol = confirm(" would you like to use symbols?")
+    console.log(charactersymbol)
+    if (charactersymbol == true) {
+      console.log(charactersymbol);
+      // add to allChars
+      allChars = allChars.concat(symbolArr)
+      password += symbolArr[Math.floor(Math.random() * symbolArr.length)]
+
+    }
+    // // when all prompts are answered then a password is generated that matches the selceted criteria
+
+    while (charLength > password.length) {
+      password += allChars[Math.floor(Math.random() * allChars.length)];
+      console.log(password);
+    };
+
+    // For the below, look into confirm(). It is like alert() and prompt(), but just for Y/N questions
+
+    return password;
   }
-  // prompt for special chars?
-
-  var charactersymbol = confirm(" would you like to use symbols?")
-  console.log(charactersymbol)
-  if (charactersymbol == false) {
-    console.log(charactersymbol);
-    // add to allChars
-    allChars = allChars.concat(symbolArr)
-    password += symbolArr[Math.floor(Math.random() * symbolArr.charLength)]
-
-  }
-  // when all prompts are answered then a password is generated that matches the selceted criteria
-
-  while (charLength > password.length) {
-    password += allChars[Math.floor(Math.random() * allChars.length)];
-  }
-
-  // For the below, look into confirm(). It is like alert() and prompt(), but just for Y/N questions
-
-  return password;
 }
 
 // Write password to the #password input
